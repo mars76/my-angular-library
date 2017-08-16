d38deac99af3f2ae50e25d725552a0b9e697629b
sateesh <kommineni.sateesh@gmail.com>
2017-08-16 02:11:46 -0400

    initial commit

--- src/components/sample.component.ts
+++ src/components/sample.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'sample-component',
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
 })
 export class SampleComponent {
 
+  @Input()
+  name:string;
   constructor() {
   }
 
